7d0f21f11dd57427076c79c1702411e8a81ee0f1
lukasz-staniszewski <lukasz.staniszewski.stud@pw.edu.pl>
2021-05-25 15:47:36 -0700

    daj spac

--- tests/test_mask_detector.py
+++ tests/test_mask_detector.py
@@ -2,31 +2,27 @@ import pytest
 from face_mask_detector import FaceMaskDetector
 from parameters import FACE_DETECTOR_PATH, SAVE_DIR, IMG_HEIGHT, IMG_WIDTH
 import cv2
-import os
 
-# mask_detector = FaceMaskDetector(
-#     face_detector_path=FACE_DETECTOR_PATH, model_path=SAVE_DIR
-# )
 mask_detector = FaceMaskDetector(
-    face_detector_path=os.path.abspath(os.getcwd())+"/haar-classifier/haarcascade_frontalface_default.xml", model_path=os.path.abspath(os.getcwd())+"/model/"
+    face_detector_path=FACE_DETECTOR_PATH, model_path=SAVE_DIR
 )
-images_path = os.path.abspath(os.getcwd()) + "/images/"
+
 
 @pytest.mark.parametrize(
     "test_img_path, expected",
     [
-        (images_path+"man_with_mask.jpg", "with_mask"),
-        (images_path+"man_with_maskv2.jpg", "with_mask"),
-        (images_path+"woman_with_mask.jpg", "with_mask"),
-        (images_path+"woman_with_maskv2.jpg", "with_mask"),
-        (images_path+"woman_with_maskv3.jpg", "with_mask"),
-        (images_path+"woman_with_maskv4.jpg", "with_mask"),
-        (images_path+"messi.jpg", "without_mask"),
-        (images_path+"ronaldo.jpg", "without_mask"),
-        (images_path+"radcliffe_without_mask.jpg", "without_mask"),
-        (images_path+"watson_without_mask.jpg", "without_mask"),
-        (images_path+"ed_without_mask.jpg", "without_mask"),
-        (images_path+"popek_without_mask.jpg", "without_mask"),
+        ("./images/man_with_mask.jpg", "with_mask"),
+        ("./images/man_with_maskv2.jpg", "with_mask"),
+        ("./images/woman_with_mask.jpg", "with_mask"),
+        ("./images/woman_with_maskv2.jpg", "with_mask"),
+        ("./images/woman_with_maskv3.jpg", "with_mask"),
+        ("./images/woman_with_maskv4.jpg", "with_mask"),
+        ("./images/messi.jpg", "without_mask"),
+        ("./images/ronaldo.jpg", "without_mask"),
+        ("./images/radcliffe_without_mask.jpg", "without_mask"),
+        ("./images/watson_without_mask.jpg", "without_mask"),
+        ("./images/ed_without_mask.jpg", "without_mask"),
+        ("./images/popek_without_mask.jpg", "without_mask"),
     ],
 )
 def test_model(test_img_path, expected):
